8bc2d4227d1f654d6a4e3846641cd9c5d41ae302
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-04-03 04:36:51 +0530

    like method

---
+++ Like.sql
@@ -0,0 +1,15 @@
+SELECT * FROM movies WHERE genre LIKE 'Comedy';
+
+
+
+ 182 | Napoleon Dynamite                              | Comedy | 2020-06-18   | true
+ 189 | Married to the Mob                             | Comedy | 2020-08-27   | true
+ 192 | Farmer's Daughter, The                         | Comedy | 2021-01-08   | false
+ 200 | C'mon Man                                      | Comedy | 2020-10-14   | true
+ 208 | Let It Ride                                    | Comedy | 2020-05-07   | false
+ 217 | Goat, The                                      | Comedy | 2020-10-05   | true
+ 240 | Nature Calls                                   | Comedy | 2021-01-18   | true
+ 241 | Visioneers                                     | Comedy | 2021-02-09   | true
+ 
+
+/* a very good method to filter emails on basis of their addresses like gmai;, hotmail,etc.. */
