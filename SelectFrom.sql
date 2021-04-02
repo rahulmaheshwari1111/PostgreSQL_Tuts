58bd3d59cd7081a99a9484ac26fcb01829203b79
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-04-03 04:40:14 +0530

    select from method

---
+++ SelectFrom.sql
@@ -0,0 +1,10 @@
+/* To read all data from movies table*/
+
+
+SELECT * FROM movies      /* select * means select every coloumn of table */
+
+SELECT name FROM movies   /*will select name*/
+
+SELECT name, genre FROM movies  /* will give these two coloumns*/
+
+
