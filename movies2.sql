bda9a9b5077fe2cb6882ed101d7ba4490266c555
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2021-04-03 04:38:33 +0530

    second table created and injected from mockaroo

---
+++ movies2.sql
@@ -0,0 +1,37 @@
+create table movies2 (
+	id INT,
+	name2 VARCHAR(50),
+	genre2 VARCHAR(50),
+	date_release DATE,
+	eng_dubbed VARCHAR(50)
+);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (1, 'Jerichow', 'Drama', '2021-02-15', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (2, 'Koch', 'Documentary', '2020-05-31', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (3, 'Student of the Year', 'Comedy|Romance', '2020-06-10', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (4, 'Lust for Life', 'Drama', '2020-09-17', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (5, 'Hour of the Furnaces, The (Hora de los hornos, La)', 'Documentary|War', '2020-11-24', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (6, 'Booty Call', 'Comedy|Romance', '2020-07-16', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (7, 'Our Town', 'Drama|Romance', '2020-09-21', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (8, 'Girl with the Dragon Tattoo, The', 'Drama|Thriller', '2020-09-03', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (9, 'Helter Skelter', 'Crime|Drama|Thriller', '2020-09-13', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (10, 'Serbian Film, A (Srpski film)', 'Horror|Thriller', '2021-01-14', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (11, 'Fuck You, Goethe (Fack Ju Göhte)', 'Comedy', '2020-07-17', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (12, 'Kiss, The', 'Drama|Romance', '2020-10-23', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (13, 'North & South', 'Drama|Romance', '2021-01-22', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (14, 'First Day of the Rest of Your Life, The (Le premier jour du reste de ta vie)', 'Comedy|Drama', '2020-11-12', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (15, 'Mockingbird Don''t Sing', 'Drama', '2020-11-06', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (16, 'January Man, The', 'Comedy|Crime|Mystery|Thriller', '2020-06-21', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (17, 'Deceptive Practice: The Mysteries and Mentors of Ricky Jay', 'Documentary', '2020-12-25', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (18, 'Good Men, Good Women (Hao nan hao nu)', 'Drama|Romance', '2021-02-23', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (19, 'I Love You Again', 'Comedy|Drama', '2020-09-12', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (20, 'Sunrise: A Song of Two Humans', 'Drama|Romance', '2021-02-22', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (21, 'North Avenue Irregulars, The', 'Action|Crime', '2020-12-11', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (22, 'Alice in Wonderland', 'Children|Fantasy|Sci-Fi', '2020-11-18', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (23, 'Wish Upon a Star', 'Comedy', '2021-03-14', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (24, 'Dying at Grace', 'Documentary', '2021-02-21', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (25, 'Memory of a Killer, The (Zaak Alzheimer, De)', 'Action|Crime|Drama|Thriller', '2021-02-19', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (26, 'Mr. Turner', 'Drama', '2020-09-19', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (27, 'Sisters of the Gion (Gion no shimai)', 'Drama', '2021-01-18', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (28, 'Unfinished Sky', 'Drama|Romance', '2020-11-06', true);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (29, 'Funny Bones', 'Comedy|Drama', '2020-05-27', false);
+insert into movies2 (id, name2, genre2, date_release, eng_dubbed) values (30, 'Theatre Bizarre, The', 'Horror', '2021-02-07', true);
